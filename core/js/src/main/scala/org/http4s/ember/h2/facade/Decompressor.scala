2df2b7f35253e931d632591f7beb81b527a329bd
Arman Bilge <armanbilge@gmail.com>
2021-12-03 04:38:24 +0000

    Fix JSImport

--- core/js/src/main/scala/org/http4s/ember/h2/facade/Decompressor.scala
+++ core/js/src/main/scala/org/http4s/ember/h2/facade/Decompressor.scala
@@ -4,7 +4,7 @@ import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
 
 @js.native
-@JSImport("hpack.js", "Compressor")
+@JSImport("hpack.js", "Decompressor")
 private[h2] class Decompressor(options: HpackOptions) extends js.Object {
   def write(raw: js.typedarray.Uint8Array): Unit = js.native
   def execute(): Unit = js.native
